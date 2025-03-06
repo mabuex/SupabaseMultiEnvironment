62cb43cec6bd084fc3ace97807f0f7685e14dd2b
Marcus Buexenstein <mabuex@gmail.com>
2025-03-06 10:17:23 +0900

    Initial Commit

---
+++ SupabaseMultiEnvironment/SupabaseMultiEnvironmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SupabaseMultiEnvironmentApp.swift
+//  SupabaseMultiEnvironment
+//
+//  Created by Marcus Buexenstein on 3/6/25.
+//
+
+import SwiftUI
+
+@main
+struct SupabaseMultiEnvironmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
